8835fcae5e5ff4b081bfc704f88281252515893e
AyaneSHOJI <shojiayane@gmail.com>
2022-06-14 11:09:20 +0200

    p.345 working with queues

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -39,6 +39,7 @@ static void WorkingWithLists()
 
 WorkingWithLists();
 
+// p.343
 static void WorkingWithDictionnaries()
 {
     Dictionary<string, string> keywords = new();
@@ -63,3 +64,72 @@ static void WorkingWithDictionnaries()
 }
 
 WorkingWithDictionnaries(); 
+
+// p.344 Working with queues
+static void WorkingWithQueues()
+{
+    Queue<string> coffee = new Queue<string>();
+
+    coffee.Enqueue("Damir"); // front of queue
+    coffee.Enqueue("Andrea");
+    coffee.Enqueue("Ronald");
+    coffee.Enqueue("Amin");
+    coffee.Enqueue("Irina"); // back of queue
+
+    Output("Initial queue from front to back", coffee);
+
+    // server handles next person in queue
+    string served = coffee.Dequeue();
+    WriteLine($"Served : {served}");
+
+    // server handles next person in queue
+    served = coffee.Dequeue();
+    WriteLine($"Served : {served}");
+
+    Output("Current queue from front to back", coffee);
+
+    WriteLine($"{coffee.Peek()} is next in line.");
+
+    Output("Current queue from front to back", coffee);
+}
+
+WorkingWithQueues();
+
+static void OutputPQ<TElement, TPriority>(string title, IEnumerable<(TElement Element, TPriority Priority)> collection)
+{
+    WriteLine(title);
+    foreach((TElement, TPriority) item in collection)
+    {
+        WriteLine($"    {item.Item1}:{item.Item2}");    
+    }
+}
+
+static void WorkingWithPriorityQueues()
+{
+    PriorityQueue<string, int> vaccine = new();
+
+    // add some people
+    // 1 = high priority people in their 70s or poor health
+    // 2 = medium priority e.g. middle aged
+    // 3 = low priority e.g. teens and twenties
+    vaccine.Enqueue("Pamela", 1); // 70s
+    vaccine.Enqueue("Rebecca", 3); // teens
+    vaccine.Enqueue("Juliet", 2); // 40s
+    vaccine.Enqueue("Ian", 1); // 70s
+
+    OutputPQ("Current queue for vaccination", vaccine.UnorderedItems);
+
+    WriteLine($"{vaccine.Dequeue()} has been vaccinated.");
+    WriteLine($"{vaccine.Dequeue()} has been vaccinated.");
+
+    OutputPQ("Current queue for vaccination", vaccine.UnorderedItems);
+
+    WriteLine($"{vaccine.Dequeue()} has been vaccinated.");
+
+    vaccine.Enqueue("Mark", 2); // 40s
+    WriteLine($"{vaccine.Peek()} will be next to be vaccinated.");
+
+    OutputPQ("Current queue for vaccination:", vaccine.UnorderedItems);
+}
+
+WorkingWithPriorityQueues();
